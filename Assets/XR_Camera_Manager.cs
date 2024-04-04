f367432423f629f40541c1e01bf09a481d771a34
Tithi Patel <patelt77@mcmaster.ca>
2024-04-04 14:12:55 -0400

    Camera settings updated to make car available

--- Assets/XR_Camera_Manager.cs
+++ Assets/XR_Camera_Manager.cs
@@ -6,15 +6,16 @@ public class XR_Camera_Manager : MonoBehaviour
 {
     public GameObject XROrigin;
     // Start is called before the first frame update
+
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        XROrigin.transform.position = new Vector3(this.transform.position.x+0.01f, this.transform.position.y - 0.06f, this.transform.position.z + 0.39f);
-        
+        XROrigin.transform.position = new Vector3(this.transform.position.x + 0.01f, this.transform.position.y - 0.06f, this.transform.position.z + 0.39f);
+
     }
 }
